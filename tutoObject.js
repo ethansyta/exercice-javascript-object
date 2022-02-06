42c43ea4a9f3340ee3d6b22cf48124318589dc01
ethansyta <96329583+ethansyta@users.noreply.github.com>
2022-02-06 13:55:22 +0100

    Add files via upload

---
+++ tutoObject.js
@@ -0,0 +1,14 @@
+class book{
+    constructor(title, author, years)
+    {
+        this.title = title;
+        this.author = author;
+        this.years = years;
+    }
+}
+
+let newBook = new book(dodo,jojo,2022);
+
+let aureBook= new book(ok, aurelie, 2020);
+
+let listBook[newbook,aureBook];
